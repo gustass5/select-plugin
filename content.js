e6ce9cb3ffc39bc13a2a1cc7b347d9174b696cf9
Gustas <gustass1@gmail.com>
2020-08-23 17:18:25 +0200

    feat: added proper comments

--- content.js
+++ content.js
@@ -1,8 +1,27 @@
+/**
+ * States if plugin is enabled
+ */
 let enabled = true;
+/**
+ * States if only exact words should be matched
+ */
 let matchExact = false;
+/**
+ * States if words should be matched by character casing
+ */
 let matchCase = false;
+/**
+ * All highlighted elements
+ */
 let highlightedElements = [];
+/**
+ * Invalid characters that will not trigger select matching
+ * For example, ' '
+ */
 let invalidChars = /\s/;
+/**
+ * Default styles
+ */
 let styles = {
   currentColor: "#ade6e6",
   color: "#ade6e6",
@@ -10,7 +29,9 @@ let styles = {
   borderColor: "black",
   borderStyle: "solid",
 };
-
+/**
+ * Gets existing styling and select options from storage
+ */
 chrome.storage.sync.get(
   ["styles", "enabled", "matchExact", "matchCase"],
   (data) => {
@@ -21,6 +42,9 @@ chrome.storage.sync.get(
   }
 );
 
+/**
+ * Listens for events
+ */
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   switch (request.name) {
     case "togglePlugin":
@@ -78,20 +102,30 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       break;
   }
 
+  /**
+   * Saves styling and select options in storage
+   */
   chrome.storage.sync.set({ styles, enabled, matchExact, matchCase });
 });
 
+/**
+ * Starts matching on double click select
+ */
 document.addEventListener("dblclick", () => {
   if (enabled) {
     getSelectionText();
   }
 });
 
+/**
+ * Resets highlighted elements after deselect
+ */
 document.addEventListener("selectionchange", () => {
   if (highlightedElements.length !== 0) {
     clearHighlightedElements();
   }
 });
+
 function getSelectionText() {
   var text = "";
   if (window.getSelection) {
@@ -99,7 +133,11 @@ function getSelectionText() {
   } else if (document.selection && document.selection.type != "Control") {
     text = document.selection.createRange().text;
   }
+
   if (text) {
+    /**
+     * Select options are applied
+     */
     if (!invalidChars.test(text)) {
       const caseFlag = matchCase ? "" : "i";
 
@@ -112,7 +150,6 @@ function getSelectionText() {
   } else {
     clearHighlightedElements();
   }
-  // console.log("Elements: ", highlightedElements);
 }
 
 function walk(node, targetRe) {
@@ -132,35 +169,45 @@ function walk(node, targetRe) {
 
 function handleText(node, targetRe) {
   var match, targetNode, followingNode, wrapper;
-  // Does the text contain our target string?
+  /**
+   * Check if match was found
+   */
   match = targetRe.exec(node.nodeValue);
   if (match) {
-    // console.log("===========");
-    // console.log({ match });
-    // Split at the beginning of the match
+    /**
+     * Split at the beginning of the match
+     */
     targetNode = node.splitText(match.index);
-    // console.log("targetNode: ", targetNode.parentNode);
-    // console.log(targetNode);
-    // Split at the end of the match.
-    // match[0] is the full text that was matched.
+    /**
+     * Split at the end of the match.
+     * match[0] is the full text that was matched.
+     */
     followingNode = targetNode.splitText(match[0].length);
+    /**
+     * Check if target node already is highlighted
+     */
     if (!targetNode.parentNode.classList.contains("$select-matched")) {
-      // console.log("followingNode: ", followingNode);
-      // Wrap the target in an `span` element with an `arabic` class.
-      // First we create the wrapper and insert it in front
-      // of the target text. We use the first capture group
-      // as the `href`.
+      /**
+       * Wrap the target in an `span` element and apply styling
+       * First we create the wrapper and insert it in front
+       * of the target text. We use the first capture group as the `href`.
+       */
       wrapper = document.createElement("span");
       applyStyles(wrapper);
       wrapper.classList.add("$select-matched");
       targetNode.parentNode.insertBefore(wrapper, targetNode);
-      // Now we move the target text inside it
+
+      /**
+       * Now we move the target text inside it
+       */
       wrapper.appendChild(targetNode);
       highlightedElements.push(wrapper);
     }
 
-    // Clean up any empty nodes (in case the target text
-    // was at the beginning or end of a text node)
+    /**
+     * Clean up any empty nodes (in case the target text
+     * was at the beginning or end of a text node)
+     */
     if (node.nodeValue.length == 0) {
       node.parentNode.removeChild(node);
     }
@@ -168,10 +215,13 @@ function handleText(node, targetRe) {
       followingNode.parentNode.removeChild(followingNode);
     }
 
-    // Continue with the next match in the node, if any
+    /**
+     * Continue with the next match in the node, if any
+     */
     match = followingNode ? targetRe.exec(followingNode.nodeValue) : null;
   }
 }
+
 function clearSelection() {
   if (window.getSelection) {
     window.getSelection().removeAllRanges();
@@ -186,6 +236,7 @@ function clearHighlightedElements() {
   });
   highlightedElements = [];
 }
+
 function applyStyles(element) {
   element.style.backgroundColor = styles.currentColor;
   element.style.outlineWidth = styles.borderSize;
